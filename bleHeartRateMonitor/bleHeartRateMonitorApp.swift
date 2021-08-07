fd1f67c912172d27bdf149fa71eaa359fb60248b
Toshiki Nagahama <exevolute@gmail.com>
2021-08-07 22:43:33 +0900

    Initial Commit

---
+++ bleHeartRateMonitor/bleHeartRateMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  bleHeartRateMonitorApp.swift
+//  bleHeartRateMonitor
+//
+//  Created by Toshiki Nagahama on 2021/08/07.
+//
+
+import SwiftUI
+
+@main
+struct bleHeartRateMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
